62a4931b0f490b09431454f8bb66f15529436a96
Kevin Bullaughey <bullaugh@sas.upenn.edu>
2020-01-30 13:13:36 -0500

    refactoring to allow batches with intermediate output written to the progress file

--- fqmultioverlap.go
+++ fqmultioverlap.go
@@ -12,7 +12,7 @@ import (
 	"sync"
 )
 
-/* This program takes a reference sample, and a file lising other fastq files
+/* This program takes a reference sample, and a file listing other fastq files
  * and outputs the sequences and the samples containing them for any of the
  * reference sample's sequences that are found in the other samples.
  *
@@ -25,19 +25,21 @@ type Args struct {
 	Ref2      string
 	FastqList string
 	Limit     int
-	Progress  bool
+	Batches   int
+	Progress  string
 }
 
 var args = Args{}
 
-// We keep track of sequences we're looking for. After the reference seqeunce
-// is read in, this will not be written to again. And in particular, it's
-// treated as read-only by the goroutines, and so simultaneous access is okay.
-var sampleSequences map[string]bool
+// We keep track of sequences we're looking for by storing the mate 1 read
+// name. After the reference seqeunce is read in, this will not be written to
+// again. And in particular, it's treated as read-only by the goroutines, and
+// so simultaneous access is okay.
+var sampleSequences map[string]string
 
 // This should only be accessed when setting up the initial set of keys and by
 // the reader goroutine.
-var refseq map[string]string
+var refseq map[string]map[string]int
 
 /* Provide an ambidexterous interface to files to read that may be gzipped */
 type AmbiReader struct {
@@ -126,10 +128,10 @@ func (r *PairedEndReader) Close() error {
 	return nil
 }
 
-// Returns a slice of two strings and no error if it reads a fastq entry.
-// On EOF it returns an empty slice.
+// Returns a slice of four strings (mate 1 name, seq 1, mate 2 name, seq 2) and
+// no error if it reads a fastq entry. On EOF it returns an empty slice.
 func (r *PairedEndReader) Read() ([]string, error) {
-	mates := make([]string, 2)
+	record := make([]string, 4)
 	var leftMate, rightMate string
 	for i := 0; i < 4; i++ {
 		if r.scanner1.Scan() {
@@ -146,10 +148,13 @@ func (r *PairedEndReader) Read() ([]string, error) {
 				if !strings.HasPrefix(rightMate, "@") {
 					return nil, fmt.Errorf("expecting %s line %d to start with '@'", r.fn2, r.lineNum+1)
 				}
+				// Store the read names, stripping off the '@' character
+				record[0] = leftMate[1:]
+				record[2] = rightMate[1:]
 			}
 			if i == 1 {
-				mates[0] = leftMate
-				mates[1] = rightMate
+				record[1] = leftMate
+				record[3] = rightMate
 				r.Records++
 			}
 			if i == 2 {
@@ -165,7 +170,7 @@ func (r *PairedEndReader) Read() ([]string, error) {
 		}
 		r.lineNum++
 	}
-	return mates, nil
+	return record, nil
 }
 
 func init() {
@@ -174,7 +179,8 @@ func init() {
 	flag.StringVar(&args.Ref2, "ref2", "", "fastq file for read 2 of the reference sample")
 	flag.StringVar(&args.FastqList, "files", "", "file that contains the list of other fastq files")
 	flag.IntVar(&args.Limit, "limit", 0, "only consider the first LIMIT fastq records in each sample")
-	flag.BoolVar(&args.Progress, "progress", false, "show progress")
+	flag.IntVar(&args.Batches, "batches", 1, "process files in batches to avoid open file limits")
+	flag.StringVar(&args.Progress, "progress", "", "write data after each batch to this file")
 
 	flag.Usage = func() {
 		log.Println("usage: fqseq [options]")
@@ -193,14 +199,17 @@ func scanFastQ(sampleId, fn1, fn2 string, ch chan string, wg *sync.WaitGroup) {
 	defer sample.Close()
 
 	for {
-		mates, err := sample.Read()
+		record, err := sample.Read()
 		if err != nil {
 			log.Fatal("Failed reading from sample %s fastq at record %d: %v", sampleId, sample.Records, err)
 		}
-		if len(mates) == 0 {
+		if len(record) == 0 {
 			break
 		}
-		key := mates[0] + ":" + mates[1]
+		if len(record) != 4 {
+			log.Fatalf("record should have 4 fields, got %d", len(record))
+		}
+		key := record[1] + ":" + record[3]
 		_, present := sampleSequences[key]
 		if present {
 			serialized := key + "@" + sampleId
@@ -222,14 +231,24 @@ func recordSamples(hits chan string, done chan int) {
 		}
 		key := tuple[0]
 		sampleId := tuple[1]
-		sep := ""
-		if len(refseq[key]) > 0 {
-			sep = ","
-		}
-		refseq[key] = refseq[key] + sep + sampleId
+		refseq[key][sampleId]++
 		numHits++
 	}
 	done <- numHits
+}
+
+func writeOutput(fp io.Writer) {
+	for key, sampleSet := range refseq {
+		if len(sampleSet) > 0 {
+			pieces := strings.Split(key, ":")
+			readName := sampleSequences[key]
+			list := make([]string, 0)
+			for sampleId, _ := range sampleSet {
+				list = append(list, sampleId)
+			}
+			fmt.Fprintf(fp, "%s\t%s\t%s\t%s\n", readName, pieces[0], pieces[1], strings.Join(list, ","))
+		}
+	}
 }
 
 func main() {
@@ -269,19 +288,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to open reference fastq files: %v", err)
 	}
-	refseq = make(map[string]string, 0)
-	sampleSequences = make(map[string]bool, 0)
+	refseq = make(map[string]map[string]int, 0)
+	sampleSequences = make(map[string]string, 0)
 	for {
-		mates, err := ref.Read()
+		record, err := ref.Read()
 		if err != nil {
 			log.Fatal("Failed reading reference fastq: %v", err)
 		}
-		if len(mates) == 0 {
+		if len(record) == 0 {
 			break
 		}
-		key := mates[0] + ":" + mates[1]
-		refseq[key] = ""
-		sampleSequences[key] = true
+		if len(record) != 4 {
+			log.Fatalf("ref record should have 4 fields, got %d", len(record))
+		}
+		key := record[1] + ":" + record[3]
+		refseq[key] = make(map[string]int, 0)
+		sampleSequences[key] = record[0]
 		if args.Limit > 0 && ref.Records >= args.Limit {
 			log.Println("Warning: reached refseq limit")
 			break
@@ -289,26 +311,44 @@ func main() {
 	}
 	ref.Close()
 
-	// Wait until all our goroutines are done
-	var wg sync.WaitGroup
-	wg.Add(len(fastqFiles))
-
 	// Make a channel that all our fastq goroutines will write to
 	hits := make(chan string)
 
 	// Make another channel that we'll wait on to detect that our reader has finished.
 	done := make(chan int)
 
-	for _, tuple := range fastqFiles {
-		go scanFastQ(tuple[0], tuple[1], tuple[2], hits, &wg)
-	}
-
 	// Lanuch a goroutine to read from the channel. This goroutine will be done
 	// when the channel is closed.
 	go recordSamples(hits, done)
 
-	// Wait for all the goroutines to be done after which we close the channel.
-	wg.Wait()
+	for b := 0; b < args.Batches; b++ {
+		thisBatch := 0
+
+		// Wait until all our goroutines in this batch are done
+		var wg sync.WaitGroup
+		for i, tuple := range fastqFiles {
+			if i%args.Batches == b {
+				go scanFastQ(tuple[0], tuple[1], tuple[2], hits, &wg)
+				wg.Add(1)
+				thisBatch++
+			}
+		}
+		// Wait for all the goroutines to be done after which we close the channel.
+		log.Printf("Processing %d samples in batch %d\n", thisBatch, b)
+		wg.Wait()
+		// Write intermediate progress, unless we're on the last batch
+		if args.Progress != "" && b != args.Batches-1 {
+			fp, err := os.Create(args.Progress)
+			if err == nil {
+				log.Println("writing intermediate progress to", args.Progress)
+				writeOutput(fp)
+				fp.Close()
+			} else {
+				log.Println("can't write to", args.Progress, "skipping")
+			}
+		}
+	}
+
 	close(hits)
 
 	// Now wait for our reader to be done.
@@ -316,12 +356,7 @@ func main() {
 	log.Println("Got", numHits, "hits")
 
 	log.Println("Writing output")
-	for key, list := range refseq {
-		if list != "" {
-			pieces := strings.Split(key, ":")
-			fmt.Printf("%s\t%s\t%s\n", pieces[0], pieces[1], list)
-		}
-	}
+	writeOutput(os.Stdout)
 }
 
 // END
